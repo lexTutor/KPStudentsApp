0eba5b7c9ec4c134ab40de94b9725eb25ec97ed8
Chibuikem Akpaka <chibuikem.akpaka@venturegardengroup.com>
2023-03-04 22:33:44 +0100

    Setup Application Services

---
+++ KPStudentsApp.Application/Services/CourseService.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using KPStudentsApp.Application.Interfaces;
+using KPStudentsApp.Domain.Entities;
+using KPStudentsApp.Infrastructure.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace KPStudentsApp.Application.Services
+{
+    public class CourseService : ICourseService
+    {
+        private readonly IGenericRepository<Course> _courseRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<CourseService> _logger;
+
+        public CourseService(
+            IGenericRepository<Course> courseRepository,
+            IMapper mapper,
+            ILogger<CourseService> logger)
+        {
+            _mapper = mapper;
+            _logger = logger;
+            _courseRepository = courseRepository;
+        }
+    }
+}
---
+++ KPStudentsApp.Application/Services/StudentService.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using KPStudentsApp.Application.Interfaces;
+using KPStudentsApp.Domain.Entities;
+using KPStudentsApp.Infrastructure.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace KPStudentsApp.Application.Services
+{
+    public class StudentService : IStudentService
+    {
+        private readonly IGenericRepository<Student> _studentRepository;
+        private readonly IGenericRepository<Course> _courseRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<StudentService> _logger;
+
+        public StudentService(
+            IGenericRepository<Student> studentRepository,
+            IGenericRepository<Course> courseRepository,
+            IMapper mapper,
+            ILogger<StudentService> logger)
+        {
+            _studentRepository = studentRepository;
+            _mapper = mapper;
+            _logger = logger;
+            _courseRepository = courseRepository;
+        }
+    }
+}
