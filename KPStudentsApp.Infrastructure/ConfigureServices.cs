84ff38ae20a8c9b121d0c106798c60b4553ecbba
Chibuikem Akpaka <chibuikem.akpaka@venturegardengroup.com>
2023-03-04 20:54:47 +0100

    Configure Application Infrastructure, inlcuding database setup.

---
+++ KPStudentsApp.Infrastructure/ConfigureServices.cs
@@ -0,0 +1,24 @@
+﻿using KPStudentsApp.Infrastructure.Persistence;
+using KPStudentsApp.Infrastructure.Persistence.Interceptors;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KPStudentsApp.Infrastructure
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<EntitySaveChangesInterceptor>();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                    builder => builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddScoped<ApplicationDbContextInitialiser>();
+
+            return services;
+        }
+    }
+}
---
+++ KPStudentsApp.Infrastructure/Persistence/Interceptors/EntitySaveChangesInterceptor.cs
@@ -0,0 +1,42 @@
+﻿using KPStudentsApp.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace KPStudentsApp.Infrastructure.Persistence.Interceptors
+{
+    public class EntitySaveChangesInterceptor : SaveChangesInterceptor
+    {
+        public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+        {
+            UpdateEntities(eventData.Context);
+
+            return base.SavingChanges(eventData, result);
+        }
+
+        public override ValueTask<InterceptionResult<int>> SavingChangesAsync
+            (DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+        {
+            UpdateEntities(eventData.Context);
+
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+
+        public void UpdateEntities(DbContext? context)
+        {
+            if (context == null) return;
+
+            foreach (var entry in context.ChangeTracker.Entries<BaseEntity>())
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.CreatedAt = DateTime.UtcNow;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Entity.UpdatedAt = DateTime.UtcNow;
+                }
+            }
+        }
+    }
+}
