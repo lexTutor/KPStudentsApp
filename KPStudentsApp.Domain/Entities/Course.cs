c53b37d491a15d6f9f40b05c4374abb9276b821a
Chibuikem Akpaka <chibuikem.akpaka@venturegardengroup.com>
2023-03-04 20:15:21 +0100

    Add Entities

---
+++ KPStudentsApp.Domain/Entities/Course.cs
@@ -0,0 +1,12 @@
+﻿using KPStudentsApp.Domain.Common;
+
+namespace KPStudentsApp.Domain.Entities
+{
+    public class Course : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public int CreditUnit { get; set; }
+        public virtual ICollection<Student> Students { get; set; }
+    }
+}
---
+++ KPStudentsApp.Domain/Entities/ReferenceNumber.cs
@@ -0,0 +1,10 @@
+﻿using KPStudentsApp.Domain.Common;
+
+namespace KPStudentsApp.Domain.Entities
+{
+    public class ReferenceNumber : BaseEntity
+    {
+        public string Dimension { get; set; }
+        public long NextVal { get; set; }
+    }
+}
